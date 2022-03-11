6a5e352d004c9c239086ea3ca406cad0a0924816
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-03-11 09:28:14 -0600

    made the widget functional

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -17,7 +17,7 @@ export default function AppFunctional(props) {
   }
 
   const downwards = () => {
-    vertical < 3 ?
+    vert < 3 ?
     ( setVert(prev => prev + 1),
       setCount(prev => prev + 1),
       setMoveError(''))
@@ -71,33 +71,57 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({hoz}, {vert})</h3>
+        <h3 id="steps">You moved {count} {count === 1? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <div className={hoz === 1 && vert === 1 ? 'square active': 'square'}>
+          { hoz === 1 & vert === 1 ? 'B': '' }
+        </div>
+        <div className={ hoz === 2 && vert === 1 ? 'square active': 'square' }>
+          { hoz === 2 & vert === 1 ? 'B': '' }
+        </div>
+        <div className={ hoz === 3 && vert === 1 ? 'square active': 'square' }>
+          { hoz === 3 & vert === 1 ? 'B': '' }
+        </div>
+        <div className={ hoz === 1 && vert === 2 ? 'square active': 'square' }>
+          { hoz === 1 & vert === 2 ? 'B': '' }
+        </div>
+        <div className={ hoz === 2 && vert === 2 ? 'square active': 'square' }>
+          { hoz === 2 & vert === 2 ? 'B': '' }
+        </div>
+        <div className={ hoz === 3 && vert === 2 ? 'square active': 'square' }>
+          { hoz === 3 & vert === 2 ? 'B': '' }
+        </div>
+        <div className={ hoz === 1 && vert === 3 ? 'square active': 'square' }>
+          { hoz === 1 & vert === 3 ? 'B': '' }
+        </div>
+        <div className={ hoz === 2 && vert === 3 ? 'square active': 'square' }>
+          { hoz === 2 & vert === 3 ? 'B': '' }
+        </div>
+        <div className={ hoz === 3 && vert === 3 ? 'square active': 'square' }>
+          { hoz === 3 & vert === 3 ? 'B': '' }
+        </div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{moveError}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={goLeft} id='left'>LEFT</button>
+        <button onClick={upwards} id="up">UP</button>
+        <button onClick={goRight} id="right">RIGHT</button>
+        <button onClick={downwards} id="down">DOWN</button>
+        <button onClick={resetter} id="reset">reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input 
+          id="email"
+          value='email'
+          onChange={evt => setEmail(evt.target.value)} 
+          type="email" 
+          placeholder="type email">
+        </input>
+        <input id="submit" onClick={formSubmission} type="submit"></input>
       </form>
     </div>
   )
