bf3e26cc953a98c293ec5e40ed4f6bcfe56e1286
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-03-11 09:59:08 -0600

    wrote some tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,39 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react';
+import AppFunctional from './AppFunctional';
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+beforeEach(() => {
+  render(<AppFunctional/>);
 })
+
+test('are headings visible', () => {
+  const heading =  document.querySelector('#coordinates');
+  expect(heading).toBeInTheDocument();
+})
+
+test('is up button appearing', () => {
+  const upBtn =  document.querySelector('#up');
+  expect(upBtn).toBeVisible;
+})
+
+test('is reset button visible', () => {
+  const resetButton = document.querySelector('#reset');
+  expect(resetButton).toBeVisible();
+})
+
+test('is message in document', () => {
+  const message = document.querySelector('#message');
+  expect (message).toBeInTheDocument;
+})
+
+test('is email input visible', () => {
+  const email = document.querySelector('#email');
+  expect(email).toBeVisible();
+})
+
+
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,38 +1,159 @@
 import React from 'react'
+import axios from 'axios'
+
+const intState = {
+  count: 0,
+  vert: 2,
+  hoz: 2,
+  moveError: '',
+  email: '',
+}
+
+const URL = 'http://localhost:9000/api/result';
 
 export default class AppClass extends React.Component {
+  state = intState;
+  vertController = (key, value) => {
+    key === 'up' && value > 1 ?
+      this.setState({
+        ...this.state,
+        vert: value - 1,
+        count: this.state.count + 1,
+        moveError: (''),
+      })
+        : key === 'up' && value === 1 ?
+          this.setState({
+            ...this.state,
+            moveError: 'You can no longer go upwards!'
+          })
+          : key === 'down' && value < 3 ?
+            this.setState({
+              ...this.state,
+              vert: value + 1,
+              count: this.state.count + 1,
+              moveError:'',
+            })
+          : this.setState({
+            ...this.state,
+            moveError: 'You can no longer go downwards!'
+          })
+  }
+  hozController = (key, value) => {
+    key === 'left' && value > 1 ?
+      this.setState({
+        ...this.state,
+        hoz: value - 1,
+        count: this.state.count + 1,
+        moveError: (''),
+      })
+        : key === 'left' && value === 1 ?
+          this.setState({
+            ...this.state,
+            moveError: 'You can no longer go left!'
+          })
+          : key === 'right' && value < 3 ?
+            this.setState({
+              ...this.state,
+              hoz: value + 1,
+              count: this.state.count + 1,
+              moveError:'',
+            })
+          : this.setState({
+            ...this.state,
+            moveError: 'You can no longer go right!'
+          })
+  }
+
+  resetter = () => {
+    this.setState({
+      ...this.state,
+      moveError: '',
+      email: '',
+      hoz: 2,
+      vert: 2,
+      count: 0,
+    })
+  }
+  onChange = (evt) => {
+    this.setState({email: evt.target.value})
+  }
+
+  post = evt => {
+    evt.preventDefault();
+    const newSub = {
+      x: this.state.hoz,
+      y: this.state.vert,
+      steps: this.state.count,
+      email: this.state.email,
+    }
+    axios.post(URL, newSub)
+      .then(res=> {
+        this.setState({moveError: res.data.message})
+      })
+      .catch(err => 
+        this.setState({moveError: err.response.data.message}))
+    this.setState({
+      ...this.state,
+      email: '',
+    })
+  }
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.hoz}, {this.state.vert})</h3>
+          <h3 id="steps">You moved {this.state.count} {this.state.count === 1 ? 'time' : 'times'}</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div className={this.state.hoz === 1 && this.state.vert === 1 ? 'square active': 'square'}>
+            { this.state.hoz === 1 & this.state.vert === 1 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 2 && this.state.vert === 1 ? 'square active': 'square'}>
+            { this.state.hoz === 2 & this.state.vert === 1 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 3 && this.state.vert === 1 ? 'square active': 'square'}>
+            { this.state.hoz === 3 & this.state.vert === 1 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 1 && this.state.vert === 2 ? 'square active': 'square'}>
+            { this.state.hoz === 1 & this.state.vert === 2 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 2 && this.state.vert === 2 ? 'square active': 'square'}>
+            { this.state.hoz === 2 & this.state.vert === 2 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 3 && this.state.vert === 2 ? 'square active': 'square'}>
+            { this.state.hoz === 3 & this.state.vert === 2 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 1 && this.state.vert === 3 ? 'square active': 'square'}>
+            { this.state.hoz === 1 & this.state.vert === 3 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 2 && this.state.vert === 3 ? 'square active': 'square'}>
+            { this.state.hoz === 2 & this.state.vert === 3 ? 'B': '' }
+          </div>          
+          <div className={this.state.hoz === 3 && this.state.vert === 3 ? 'square active': 'square'}>
+            { this.state.hoz === 3 & this.state.vert === 3 ? 'B': '' }
+          </div>          
         </div>
         <div className="info">
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={() => this.hozController('left', this.state.hoz)}>LEFT</button>
+          <button id="up" onClick={() => this.vertController('up', this.state.vert)}>UP</button>
+          <button id="right" onClick={() => this.hozController('right', this.state.hoz)}>RIGHT</button>
+          <button id="down" onClick={() => this.vertController('down', this.state.vert)}>DOWN</button>
+          <button id="reset" onClick={() => this.resetter()}>reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input 
+          id="email" 
+          type="email" 
+          placeholder="type email"
+          onChange={this.onChange}
+          value={this.state.email}
+          ></input>
+          <input id="submit" onClick={this.post} type="submit"></input>
         </form>
       </div>
     )
