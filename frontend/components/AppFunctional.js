ae8aaae314cbe7d47039f3ab99b50a4c111f8916
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-03-11 08:59:41 -0600

    initial commit

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function AppFunctional(props) {
+
+  
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
